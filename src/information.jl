2297aaab728ac9e87fe08102372ca123648dcda0
Meihua Dang <mhdang0026@gmail.com>
2022-01-27 19:46:26 -0800

    mi test

--- src/information.jl
+++ src/information.jl
@@ -1,25 +1,25 @@
-export pairwise_marginals_binary, marginals_binary, pairwise_MI_binary,
-pairwise_marginals, 
-pairwise_mutual_information,
-pairwise_MI,
-as_categorical_datas
+export pairwise_marginal, pairwise_MI
 
-# using Statistics
+using LinearAlgebra: diagind, diag
 using StatsFuns: xlogx, xlogy
 using CUDA: CUDA, CuMatrix, CuVector, CuArray
 using DataFrames: DataFrame
 
+
 #############################
-# Mutual Information Binary
+# Mutual Information on Binary Data
 #############################
 
-function pairwise_marginals_binary(data::Union{Matrix, CuMatrix}; weights=nothing, pseudocount = 1.0)
+function pairwise_marginal(data::Union{Matrix{Bool}, CuMatrix{Bool}, BitMatrix}; 
+        weights::Union{Vector, Nothing}=nothing, 
+        pseudocount=1.0,
+        Float=Float64)
     N = isnothing(weights) ? size(data, 1) : sum(weights)
     D = size(data, 2)
-    base = N + 4 * pseudocount
+    base = N + pseudocount
     # not_data = .!data
-    not_data = Float64.(.!data)
-    data = Float64.(data)
+    not_data = Float.(.!data)
+    data = Float.(data)
     pxy = similar(data, D, D, 4)
     
     if isnothing(weights)
@@ -31,37 +31,31 @@ function pairwise_marginals_binary(data::Union{Matrix, CuMatrix}; weights=nothin
         pxy[:, :, 3] = transpose(data) * (not_data .* weights)
         pxy[:, :, 2] = transpose(not_data) * (data .* weights)
     end
-    pxy[:, :, 1] = base .- pxy[:, :, 4] .- pxy[:, :, 3] .- pxy[:, :, 2]
+    pxy[:, :, 1] = N .- pxy[:, :, 4] .- pxy[:, :, 3] .- pxy[:, :, 2]
+    
+    joint_count = pseudocount / 4
+    pxy .+= joint_count
 
-    pxy .+= pseudocount
+    # diagonal is the marginal of p(x)
+    diag = diagind(pxy[:, :, 1])
+    @view(pxy[:,:, 1])[diag] .+= joint_count
+    @view(pxy[:,:, 2])[diag] .-= joint_count
+    @view(pxy[:,:, 3])[diag] .-= joint_count
+    @view(pxy[:,:, 4])[diag] .+= joint_count
+
     pxy ./= base
-    pxy
 end
 
 
-function marginals_binary(data::Union{Matrix, CuMatrix}; weights=nothing, pseudocount = 1.0)
-
-    N = isnothing(weights) ? size(data, 1) : sum(weights)
-    base = N + 4 * pseudocount
-    data = Float64.(data)
-    
-    if isnothing(weights)
-        count1 = dropdims(sum(data, dims=1), dims=1)
-    else
-        count1 = dropdims(sum(data .* weights, dims=1), dims=1)
-    end
-    count0 = base .- count1
-    
-
-    (count0 .+ 2 * pseudocount) / base, (count1 .+ 2 * pseudocount) / base
-end
-
-
-function pairwise_MI_binary(data::Union{Matrix, CuMatrix}; weights=nothing, pseudocount=1.0)
-    pxy = pairwise_marginals_binary(data; weights=weights, pseudocount=pseudocount)
-    p0, p1 = marginals_binary(data; weights=weights, pseudocount=pseudocount)
+function pairwise_MI(data::Union{Matrix{Bool}, CuMatrix{Bool}, BitMatrix}; 
+        weights=nothing, 
+        pseudocount=1.0,
+        Float=Float64)
+    pxy = pairwise_marginal(data; weights=weights, pseudocount=pseudocount)
+    p0 = diag(pxy[:, :, 1])
+    p1 = diag(pxy[:, :, 4])
     D = size(data, 2)
-    pxpy = Float64.(similar(data, D, D, 4))
+    pxpy = Float.(similar(data, D, D, 4))
     pxpy[:,:,1] = p0 * p0'
     pxpy[:,:,2] = p0 * p1'
     pxpy[:,:,3] = p1 * p0'
@@ -73,22 +67,34 @@ end
 
 
 #############################
-# Mutual Information Categorical
+# Mutual Information on Categorical Data
 #############################
 
-
-"Compute an array giving all pairwise marginals estimated on empirical (weighted) data"
-function pairwise_marginals(data::Matrix, weights::Vector, num_cats = maximum(data); pseudocount = 1.0)
-    
+function pairwise_marginal(data::Matrix;
+        weights::Union{Vector, Nothing}=nothing,
+        num_cats=maximum(data),
+        pseudocount=1.0,
+        Float=Float64)
     @assert minimum(data) > 0 "Categorical data labels are assumed to be indexed starting 1"
-    num_vars = size(data,2)
-    pair_margs = Array{Float32}(undef, num_vars, num_vars, num_cats, num_cats)
+
+    num_samples = size(data, 1)
+    num_vars = size(data, 2)
+
+    if weights === nothing
+        weights = ones(Int32, num_samples)
+    else
+        pseudocount = pseudocount * sum(weights) / num_samples
+    end
+
+    num_vars = size(data, 2)
+    pair_margs = Array{Float}(undef, num_vars, num_vars, num_cats, num_cats)
     Z = sum(weights) + pseudocount
     single_smooth = pseudocount / num_cats
     pair_smooth = single_smooth / num_cats
     
-    for i = 1:num_vars 
-        Threads.@threads for j = 1:num_vars # note: multithreading needs to be on inner loop for thread-safe copying across diagonal
+    for i = 1:num_vars
+        # note: multithreading needs to be on inner loop for thread-safe copying across diagonal
+        Threads.@threads for j = 1:num_vars
             if i<=j
                 if i!=j
                     @inbounds pair_margs[i,j,:,:] .= pair_smooth
@@ -113,10 +119,25 @@ function pairwise_marginals(data::Matrix, weights::Vector, num_cats = maximum(da
 end
 
 
-function pairwise_marginals(data::CuMatrix, weights::CuVector, num_cats = maximum(data); pseudocount = 1.0)
+function pairwise_marginal(data::CuMatrix; 
+        weights::Union{CuVector, Nothing}=nothing,
+        num_cats=maximum(data),
+        pseudocount=1.0,
+        Float=Float64)
+
     @assert minimum(data) > 0 "Categorical data labels are assumed to be indexed starting 1"
+
+    num_samples = size(data, 1)
+    num_vars = size(data, 2)
+
+    if weights === nothing
+        weights = ones(Int32, num_samples)
+    else
+        pseudocount = pseudocount * sum(weights) / num_samples
+    end
+
     num_vars = size(data,2)
-    pair_margs = CuArray{Float32}(undef, num_vars, num_vars, num_cats, num_cats)
+    pair_margs = CuArray{Float}(undef, num_vars, num_vars, num_cats, num_cats)
     Z = sum(weights) + pseudocount
     single_smooth = pseudocount / num_cats
     pair_smooth = single_smooth / num_cats
@@ -157,16 +178,13 @@ function pairwise_marginals(data::CuMatrix, weights::CuVector, num_cats = maximu
 end
 
 
-"Compute pairwise Mutual Information given binary/categorical data."
-function pairwise_MI(dataset::DataFrame, num_vars, num_cats; pseudocount = 1.0)
-    categorical_dataset, weights = as_categorical_data(dataset::DataFrame, num_vars, num_cats)
-    pairwise_MI(categorical_dataset, num_vars, num_cats, weights; pseudocount = pseudocount)
-end
-
-
-function pairwise_MI(dataset::Matrix, num_vars, num_cats, weights = nothing; pseudocount = 1.0)
-    num_samples = size(dataset, 1)
-    num_vars = size(dataset, 2)
+function pairwise_MI(data::Matrix;
+            weights::Union{Vector, Nothing} = nothing,
+            num_cats = maximum(data),
+            pseudocount = 1.0,
+            Float = Float64)
+    num_samples = size(data, 1)
+    num_vars = size(data, 2)
     
     if weights === nothing
         weights = ones(Int32, num_samples)
@@ -174,11 +192,8 @@ function pairwise_MI(dataset::Matrix, num_vars, num_cats, weights = nothing; pse
         pseudocount = pseudocount * sum(weights) / num_samples
     end
     
-    # Sum of the weights
-    sum_weights::Float64 = Float64(sum(weights) + num_cats^2 * pseudocount)
-    
     # `joint_cont[i, j, k, w]' is the total weight of samples whose i- and j-th variable are k and w, respectively
-    joint_cont = pairwise_marginals(dataset, weights, num_cats; pseudocount)
+    joint_cont = pairwise_marginal(data; weights, num_cats, pseudocount)
     
     # `marginal_cont[i, j]' is the total weight of sample whose i-th variable is j
     marginal_cont = zeros(Float64, num_vars, num_cats)
@@ -189,11 +204,12 @@ function pairwise_MI(dataset::Matrix, num_vars, num_cats, weights = nothing; pse
     end
     
     # Compute mutual information
-    MI = zeros(Float64, num_vars, num_vars)
+    MI = zeros(Float, num_vars, num_vars)
     for var1_idx = 1 : num_vars
         for var2_idx = var1_idx : num_vars
-            @inbounds MI[var1_idx, var2_idx] = sum(xlogy.(joint_cont[var1_idx, var2_idx, :, :], 
-            joint_cont[var1_idx, var2_idx, :, :] ./ (marginal_cont[var1_idx, :] .* marginal_cont[var2_idx, :]')))
+            @inbounds MI[var1_idx, var2_idx] = sum(
+                xlogx.(joint_cont[var1_idx, var2_idx, :, :]) .-
+                xlogy.(joint_cont[var1_idx, var2_idx, :, :], marginal_cont[var1_idx, :] .* marginal_cont[var2_idx, :]'))
         end
     end
     
@@ -206,82 +222,5 @@ function pairwise_MI(dataset::Matrix, num_vars, num_cats, weights = nothing; pse
     MI
 end
 
-function as_categorical_data(dataset::DataFrame, num_vars, num_cats)
-    
-    # Convert binary data to categorical data
-    if isweighted(dataset)
-        dataset, weights = split_sample_weights(dataset)
-        weights = convert(Vector{Float64}, weights)
-    else
-        weights = nothing
-    end
-    
-    num_samples = size(dataset, 1)
-    num_bits = ceil(Int,log2(num_cats))
-    
-    # Get categorical dataset from the binarized dataset
-    categorical_dataset = Matrix{UInt32}(undef, num_samples, num_vars)
-    if iscomplete(dataset)
-        for sample_idx = 1 : num_samples
-            for variable_idx = 1 : num_vars
-                @inbounds categorical_dataset[sample_idx, variable_idx] = as_cat(dataset[sample_idx, (variable_idx - 1) * num_bits + 1 : variable_idx * num_bits]; complete = true)
-            end
-        end
-    else # If the dataset contains missing values, we impute the missing values with the mode of each column
-        for variable_idx = 1 : num_vars
-            cat_counts::Array{UInt32} = zeros(UInt32, num_cats)
-            for sample_idx = 1 : num_samples
-                category = as_cat(dataset[sample_idx, (variable_idx - 1) * num_bits + 1 : variable_idx * num_bits]; complete = false)
-                if category != typemax(UInt32)
-                    cat_counts[category] += 1
-                end
-                @inbounds categorical_dataset[sample_idx, variable_idx] = category
-            end
-            cat_mode = argmax(cat_counts)
-            for sample_idx = 1 : num_samples
-                if categorical_dataset[sample_idx, variable_idx] == typemax(UInt32)
-                    @inbounds categorical_dataset[sample_idx, variable_idx] = cat_mode
-                end
-            end
-        end
-    end
-    return categorical_dataset, weights
-end
 
-
-isweighted(df::DataFrame) = 
-    names(df)[end] == "weight"
-isweighted(df::Vector{DataFrame}) = 
-    all(d -> isweighted(d), df)
-
-
-"Is the data complete (no missing values)?"
-iscomplete(data::DataFrame) = 
-    all(iscomplete_col, eachcol_unweighted(data))
-iscomplete(data::Vector{DataFrame}) = all(iscomplete, data)
-
-"Is the data column complete (no missing values)?"
-iscomplete_col(::AbstractVector{Bool}) = true
-iscomplete_col(::AbstractVector{<:Int}) = true
-iscomplete_col(::AbstractVector{<:AbstractFloat}) = true
-iscomplete_col(x::AbstractVector{Union{Bool,Missing}}) = !any(ismissing, x)
-iscomplete_col(x::AbstractVector{Union{<:AbstractFloat,Missing}}) = !any(ismissing, x)
-
-
-"Iterate over columns, excluding the sample weight column"
-eachcol_unweighted(data::DataFrame) = 
-    isweighted(data) ? eachcol(data)[1:end-1] : eachcol(data)
-
-
-function as_cat(bits; complete)
-    if !complete && !iscomplete(bits)
-        return typemax(UInt32)
-    end
-    
-    category::UInt32 = UInt32(0)
-    for bit_idx = length(bits) : -1 : 1
-        category = (category << 1) + bits[bit_idx]
-    end
-    
-    (category == 0) ? 2^(length(bits)) : category
-end
+# TODO categorical pairwise MI on GPU
---
+++ test/information_tests.jl
@@ -0,0 +1,92 @@
+using Test
+using ChowLiuTrees
+
+
+@testset "Pairwise marginals and mutual information for binary data" begin
+    # Binary
+    x_bit = Bool.([ 0  0  0  0
+                1  0  1  1
+                0  1  0  1
+                1  1  0  0
+                1  0  1  0
+                1  0  1  0
+                0  1  1  1
+                0  0  1  1
+                1  0  0  0
+                0  1  1  1])
+    
+    mi_bit = pairwise_MI(x_bit, pseudocount=0.0)
+
+    @test mi_bit[1,1] ≈ 0.6931471805599453
+    @test mi_bit[1,2] ≈ 0.08630462173553435
+    @test mi_bit[1,3] ≈ 0.0
+    @test mi_bit[2,2] ≈ 0.6730116670092565
+    @test mi_bit[2,3] ≈ 0.013844293808390695
+    @test mi_bit[3,3] ≈ 0.6730116670092565
+
+   
+    for pseudocount in [0.0, 1.0, 100.0]
+        for weights in [nothing, ones(4), [0.1, 0.2, 0.3, 0.4]]
+            for Float in [Float64, Float32]
+                mar1 = pairwise_marginal(x_bit, pseudocount=pseudocount)
+                mi1 = pairwise_MI(x_bit, pseudocount=pseudocount) 
+                for T in [CuArray{Bool}, Matrix{Bool}, Matrix{Int}, ] # CuArray{Int}
+                    x2 = T(x_bit)
+                    if T == Matrix{Int}
+                        x2 .+= 1
+                    end
+
+                    if T == CuArray{Bool} && !isnothing(weights)
+                        weights = CuArray(weights)
+                    end
+
+                    mar2 = pairwise_marginal(x2; pseudocount=pseudocount)
+                    mi2 = pairwise_MI(x2; pseudocount=pseudocount)
+                    
+                    if T == CuArray{Bool} 
+                        mar2 = Array(mar2)
+                        mi2 = Array(mi2)
+                    end
+
+                    if T == Matrix{Int}
+                        @test all(mar2[:, :, 1, 1] .== mar1[:, :, 1])
+                        @test all(mar2[:, :, 1, 2] .== mar1[:, :, 2])
+                        @test all(mar2[:, :, 2, 1] .== mar1[:, :, 3])
+                        @test all(mar2[:, :, 2, 2] .== mar1[:, :, 4])
+                    else
+                        @test all(mar1 .== mar2)
+                    end
+                    @test all(mi1 .≈ mi2)
+                end
+            end
+        end
+    end
+    
+end
+
+
+@testset "Pairwise marginals and mutual information for categorical data" begin
+    # Binary
+    x = [ 2  1  3  3
+        1  3  3  2
+        1  2  2  3
+        3  2  2  2
+        3  3  2  2
+        1  3  3  2
+        2  3  2  3
+        1  2  3  3
+        3  1  2  3
+        1  2  3  1]
+    
+    m = pairwise_marginal(x, pseudocount=0.0)
+    mi = pairwise_MI(x, pseudocount=0.0)
+
+    @test mi[1,1] ≈ 1.0296530140645737
+    @test mi[1,2] ≈ 0.2502012117690939
+    @test mi[1,3] ≈ 0.30431653267886216
+    @test mi[2,2] ≈ 1.054920167986144
+    @test mi[2,3] ≈ 0.0
+    @test mi[2,4] ≈ 0.3025260261455486
+
+    # TODO: compare with GPU
+end
